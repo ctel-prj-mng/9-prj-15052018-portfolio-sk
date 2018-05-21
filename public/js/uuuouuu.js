a78d63434ebd6b692a4719190a60315acf6e0f6f
Kirill Skorobogatov <Kirill9806@gmail.com>
2018-05-21 21:00:59 +0300

    Add files via upload

---
+++ public/js/uuuouuu.js
@@ -0,0 +1,41 @@
+$(function rew(){
+
+  $("#meu_div").click(function(){
+    $("#qqq").toggleClass("spinEffect");
+  });
+});
+
+$(function werty(){
+
+
+  $("#meu_div2").click(function(){
+    $("#qqq2").toggleClass("spinEffect");
+  });
+
+});
+
+
+$(function poyut(){
+
+
+  $("#meu_div3").click(function(){
+    $("#qqq3").toggleClass("spinEffect");
+  });
+});
+
+$(function poyuto(){
+
+
+  $("#meu_div4").click(function(){
+    $("#qqq4").toggleClass("spinEffect");
+  });
+});
+
+
+
+
+$("h1").slabText({
+            "viewportBreakpoint":380
+});
+
+
